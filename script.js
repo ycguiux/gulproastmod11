d7d1c67e10a747e84db3091720669e5cca77de56
Yunna Choi <yclass.uiux@gmail.com>
2025-01-16 10:16:04 -0800

    Gulp Roast Module 10 Yunna Choi

---
+++ script.js
@@ -0,0 +1,24 @@
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+
+/* color palette:
+#082B59 bright blue
+#131D26 navy blue
+#59331D coffee
+#D98555 peach
+#F2F2F2 white
+
+Fonts:
+Headlines - Poppins Bold
+Text - Lato
+
+Links all go to https://uxuicurriculum.wixsite.com/gulproast
+
+*/
+
